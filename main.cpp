b4a4ab854645ae9ce24bf9dbeba562e32632c356
HoneyPotter-Gzy <913859822@qq.com>
2022-06-17 10:52:45 +0800

    Add some issues

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+/*
+ * @Autor: Zheyuan Gu
+ * @Date: 2022-06-16 17:04:03
+ * @LastEditors: Zheyuan Gu
+ * @LastEditTime: 2022-06-17 10:52:35
+ * @Description: 
+ */
 /* ************************************************************************
 > File Name:     main.cpp
 > Author:        程序员Carl
@@ -14,13 +21,13 @@ int main() {
     // 键值中的key用int型，如果用其他类型，需要自定义比较函数
     // 而且如果修改key的类型，同时需要修改skipList.load_file函数
     SkipList<int, std::string> skipList(6);
-	skipList.insert_element(1, "学"); 
-	skipList.insert_element(3, "算法"); 
-	skipList.insert_element(7, "认准"); 
-	skipList.insert_element(8, "微信公众号：代码随想录"); 
-	skipList.insert_element(9, "学习"); 
-	skipList.insert_element(19, "算法不迷路"); 
-	skipList.insert_element(19, "赶快关注吧你会发现详见很晚！"); 
+	skipList.insert_element(1, "咕"); 
+	skipList.insert_element(3, "叽叽"); 
+	skipList.insert_element(7, "今天是"); 
+	skipList.insert_element(8, "6月17日"); 
+	skipList.insert_element(9, "今天要"); 
+	skipList.insert_element(19, "学"); 
+	skipList.insert_element(19, "java了！"); 
 
     std::cout << "skipList size:" << skipList.size() << std::endl;
 
